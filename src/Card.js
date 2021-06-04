ef3a6925444a193c57f06c4bf38117ffe33b475b
Ryan McGuire <ryan.mcguire.va@gmail.com>
2021-06-03 23:53:00 -0400

    Accessibility Fix

--- src/Card.js
+++ src/Card.js
@@ -5,10 +5,10 @@ class Card extends Component {
     render() { 
         return ( 
             <div class={`Card Card-${this.props.image}`}>
-                <img src={`/images/icon-${this.props.image}.svg`} />
+                <img src={`/images/icon-${this.props.image}.svg`} alt={`${this.props.image} vehicle`}/>
                 <h3>{this.props.image}</h3>
                 <p>{this.props.copy}</p>
-                <a href="https://www.frontendmentor.io/challenges/3column-preview-card-component-pH92eAR2-" alt="Learn More About The 3 Column Challenge">Learn More</a>
+                <a href="https://www.frontendmentor.io/challenges/3column-preview-card-component-pH92eAR2-">Learn More</a>
             </div> 
         );
     }
